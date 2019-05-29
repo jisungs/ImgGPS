6c69ca195dd6f47e302ce8d9a8377d127e854543
김해겸(111237) <111237@HITEJINRO.AD>
2019-05-29 10:29:07 +0900

    Show image's lotitude and longitude on root route

--- app.js
+++ app.js
@@ -19,25 +19,28 @@ app.post('/map', function(req, res){
 
 });
 
-try {
-   new ExifImage({ image : 'img/IMG_9826.JPG' }, function (error, exifData) {
-       if (error)
-           console.log('Error: '+error.message);
-       else
-           console.log(exifData.gps.GPSLatitude[0]);
-           const lat = exifData.gps.GPSLatitude;
-           //console.log(exifData.gps.GPSLongitude);
-
-           gpsData.push(lat);
-            // Do something with your data!
-   });
-} catch (error) {
-   console.log('Error: ' + error.message);
-}
-
 app.get('/', function(req, res){
+
   res.send(gpsData);
+
+  try {
+     new ExifImage({ image : 'img/IMG_9826.JPG' }, function (error, exifData) {
+         if (error)
+             console.log('Error: '+error.message);
+         else
+             console.log(exifData.gps.GPSLatitude[0]);
+             const lat = exifData.gps.GPSLatitude;
+             const lon = exifData.gps.GPSLongitude;
+             //console.log(exifData.gps.GPSLongitude);
+             gpsData.push(lat);
+             gpsData.push(lon);
+              // Do something with your data!
+     });
+  } catch (error) {
+     console.log('Error: ' + error.message);
+  }
 });
+
 
 app.listen("3000", function(){
   console.log("Sever is running on port 3000.");
